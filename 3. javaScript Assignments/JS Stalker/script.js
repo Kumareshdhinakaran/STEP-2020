0e5efb39ebbde22ce023e7614fa163f0d31e9d9b
kumaresh <odkumaresh@gmail.com>
2020-06-06 19:40:13 +0530

    Stalker assignment completed

---
+++ 3. javaScript Assignments/JS Stalker/script.js
@@ -0,0 +1,6 @@
+var firstName = prompt("Enter your First Name:");
+var lastName = prompt("Enter your Last Name:");
+var age = prompt("Enter your Age:");
+console.log("Your details are...");
+console.log("\t Your full name is " + firstName + " " + lastName);
+console.log("\t Your age is " + age);
