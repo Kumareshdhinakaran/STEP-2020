8c7c4dc354eddbadbd93513e84b5e12beea577e9
kumaresh <odkumaresh@gmail.com>
2020-06-06 19:45:14 +0530

    Fullname variable added in JS stalker assignment

--- 3. javaScript Assignments/JS Stalker/script.js
+++ 3. javaScript Assignments/JS Stalker/script.js
@@ -1,6 +1,7 @@
 var firstName = prompt("Enter your First Name:");
 var lastName = prompt("Enter your Last Name:");
 var age = prompt("Enter your Age:");
+var fullName = firstName + " " + lastName;
 console.log("Your details are...");
-console.log("\t Your full name is " + firstName + " " + lastName);
+console.log("\t Your full name is " + fullName);
 console.log("\t Your age is " + age);
